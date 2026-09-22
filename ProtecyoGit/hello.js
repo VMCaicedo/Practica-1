f75dabfa9e6472525b29184487d44a17053acb21
“Victor <vmanuelcaicedo1@gmail.comgit config --global user.name Victor Caicedogit config --global user.email vmanuelcaicedo1@gmail.com>
2026-09-22 13:41:48 -0600

    Organiza practicas en carpeta

--- ProtecyoGit/hello.js
+++
@@ -1,7 +0,0 @@
-let nombre = "Vicman";
-console.log("hello word", nombre);
-console.log("Soy Victor ")
-console.log("Estoy aprendiendo Java")
-console.log("Quiero crear una APP")
-
-console.log("Ejecuendado Rama login")
