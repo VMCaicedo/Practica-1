e5cd8512c7db1f7d4ff0b2cd519d6b54ffc598d4
“Victor <vmanuelcaicedo1@gmail.comgit config --global user.name Victor Caicedogit config --global user.email vmanuelcaicedo1@gmail.com>
2026-09-22 12:37:21 -0600

    Agrega labs de Generation

---
+++ ProtecyoGit/hello.js
@@ -0,0 +1,7 @@
+let nombre = "Vicman";
+console.log("hello word", nombre);
+console.log("Soy Victor ")
+console.log("Estoy aprendiendo Java")
+console.log("Quiero crear una APP")
+
+console.log("Ejecuendado Rama login")
---
+++ pf-l4-main/Contador.py
@@ -0,0 +1,9 @@
+
+
+for numero in range(1,11):
+    if numero < 5:
+        print("Numero pequeño")
+    elif numero==5:
+        print("Legamos a 5")
+    else:
+        print("Numero grande")
